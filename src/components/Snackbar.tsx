33f1d59c35cbbb833d6b05e71eb10a8ff6bc0eb1
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:31:41 +0100

    refactor(Snackbar): sizing constraints

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -5,8 +5,8 @@ export type SnackbarProps = {
 
 export function Snackbar({ text, action }: SnackbarProps) {
   return (
-    <div className="theme-invert inline-flex items-center justify-between gap-x-4 rounded bg-contrast-overlay p-4 text-content-primary shadow">
-      <div role="status" className="max-w-md flex-1 text-sm">
+    <div className="theme-invert inline-flex max-w-xl items-center justify-between gap-x-4 rounded bg-contrast-overlay p-4 text-sm text-content-primary shadow">
+      <div role="status" className="flex-1">
         {text}
       </div>
       {action != null ? <div className="-my-2">{action}</div> : null}
