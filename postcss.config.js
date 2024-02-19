d685d902e708f2c3c19d596258f3a74f22e7f08e
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:30:40 +0100

    fix: css import inlining

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const pkg = require("./package.json");
+
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies),
+].map((packageName) => new RegExp(`^${packageName}($|/)`));
+
 module.exports = {
   plugins: {
-    "postcss-import": {},
+    "postcss-import": {
+      filter: (url) => !external.some((name) => name.test(url)),
+    },
     "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
     "postcss-preset-env": {
