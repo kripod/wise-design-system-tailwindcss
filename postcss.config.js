154daf000dfc5e6542f97a33b26d1d0fdac5adb5
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:34:22 +0100

    fix(css): disable logical property transforms

--- postcss.config.js
+++ postcss.config.js
@@ -15,7 +15,15 @@ module.exports = {
     "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
     "postcss-preset-env": {
-      features: { "nesting-rules": false },
+      features: {
+        "float-clear-logical-values": false,
+        "logical-overflow": false,
+        "logical-overscroll-behavior": false,
+        "logical-properties-and-values": false,
+        "logical-resize": false,
+        "logical-viewport-units": false,
+        "nesting-rules": false,
+      },
     },
   },
 };
