f6d625a1331df3146b187441f195ef85648b55c9
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:32:15 +0100

    chore(deps): update ESLint config

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ export { renderButtonAsLink } from "./components/buttons/_Button";
 export * from "./components/buttons/ActionButton";
 export * from "./components/buttons/CircularButton";
 // TODO: export * from "./components/buttons/PrimaryButton";
-export type { PrimaryButtonProps } from "./components/buttons/PrimaryButton";
-export { PrimaryButton } from "./components/buttons/PrimaryButton";
+export {
+  PrimaryButton,
+  type PrimaryButtonProps,
+} from "./components/buttons/PrimaryButton";
 export * from "./components/buttons/SecondaryButton";
 export * from "./components/buttons/TertiaryButton";
 export * from "./components/chips/CheckboxChip";
@@ -15,8 +17,10 @@ export * from "./components/formControls/Field";
 // TODO: export * from "./components/formControls/SelectInput";
 export * from "./components/formControls/TextAreaInput";
 // TODO: export * from "./components/formControls/TextInput";
-export type { TextInputProps } from "./components/formControls/TextInput";
-export { TextInput } from "./components/formControls/TextInput";
+export {
+  TextInput,
+  type TextInputProps,
+} from "./components/formControls/TextInput";
 export * from "./components/Instruction";
 // TODO: export * from "./components/Modal";
 // TODO: export * from "./components/Tabs";
