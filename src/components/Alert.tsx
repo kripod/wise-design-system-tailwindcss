bfc2826ad9389d8668394a24bb3d6815aa78e134
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:30:51 +0100

    refactor(Alert): update as per design

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,33 +1,57 @@
-import {
-  AlertCircleFill,
-  CheckCircleFill,
-  CrossCircleFill,
-  InfoCircleFill,
-} from "@transferwise/icons";
+import { Alert as AlertIcon, Check, Cross, Info } from "@transferwise/icons";
+import { clsx } from "clsx";
 
 import { CloseButton } from "./buttons/CloseButton";
 
-export type AlertProps = {
-  sentiment?: "neutral" | "negative" | "positive" | "warning";
+type IconWrapperProps = {
+  className?: string;
   children?: React.ReactNode;
-  onClose?: () => void;
 };
+
+function IconWrapper({ className, children }: IconWrapperProps) {
+  return (
+    <span
+      className={clsx(
+        "inline-flex h-10 w-10 items-center justify-center rounded-full",
+        className,
+      )}
+    >
+      {children}
+    </span>
+  );
+}
 
 const IconBySentiment: {
   [key in NonNullable<AlertProps["sentiment"]>]: React.ComponentType<{
     [key: string]: never;
   }>;
 } = {
-  neutral: () => <InfoCircleFill size={24} />,
+  neutral: () => (
+    <IconWrapper className="bg-content-secondary">
+      <Info size={32} className="text-contrast-overlay" />
+    </IconWrapper>
+  ),
   negative: () => (
-    <CrossCircleFill size={24} className="text-sentiment-negative" />
+    <IconWrapper className="bg-sentiment-negative">
+      <Cross size={32} className="text-contrast-overlay" />
+    </IconWrapper>
   ),
   positive: () => (
-    <CheckCircleFill size={24} className="text-sentiment-positive" />
+    <IconWrapper className="bg-sentiment-positive">
+      <Check size={32} className="text-contrast-overlay" />
+    </IconWrapper>
   ),
   warning: () => (
-    <AlertCircleFill size={24} className="text-sentiment-warning" />
+    <IconWrapper className="bg-sentiment-warning">
+      <AlertIcon size={32} className="text-content-primary" />
+    </IconWrapper>
   ),
+};
+
+export type AlertProps = {
+  sentiment?: "neutral" | "negative" | "positive" | "warning";
+  children?: React.ReactNode;
+  onClose?: () => void;
 };
 
 export function Alert({
@@ -37,20 +61,22 @@ export function Alert({
 }: AlertProps) {
   const Icon = IconBySentiment[sentiment];
   return (
-    <div className="grid grid-cols-[auto_1fr_auto] gap-4 rounded-md bg-background-neutral p-6 text-sm text-content-secondary @container">
-      <Icon />
-      {onClose != null ? (
-        <CloseButton
-          size="sm"
-          className="col-start-3 self-start"
-          onClick={onClose}
-        />
-      ) : null}
-      <div
-        role="alert"
-        className="col-span-full flex flex-col items-start gap-y-2 self-center @xl:col-span-1 @xl:col-start-2 @xl:row-start-1"
-      >
-        {children}
+    <div className="@container">
+      <div className="grid grid-cols-[auto_1fr_auto] items-center gap-x-4 gap-y-2 rounded-md bg-background-neutral p-6 text-sm text-content-secondary @lg:p-4">
+        <Icon />
+        {onClose != null ? (
+          <CloseButton
+            size="sm"
+            className="col-start-3 -m-2 self-start @lg:m-0"
+            onClick={onClose}
+          />
+        ) : null}
+        <div
+          role="alert"
+          className="col-span-full flex flex-col gap-y-2 @lg:col-span-1 @lg:col-start-2 @lg:row-start-1"
+        >
+          {children}
+        </div>
       </div>
     </div>
   );
