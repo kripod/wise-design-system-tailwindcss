dcc5a39285146a83bf8f282a1c5fd1914f8bdc8f
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:31:59 +0100

    build: use TypeScript to emit declaration maps

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,8 @@
-const { babel } = require("@rollup/plugin-babel");
-const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const { getBabelOutputPlugin } = require("@rollup/plugin-babel");
 const typescript = require("@rollup/plugin-typescript");
 const { defineConfig } = require("rollup");
 
 const pkg = require("./package.json");
-
-const extensions = [".js", ".jsx", ".mjs", ".ts", ".tsx"];
 
 module.exports = defineConfig({
   input: {
@@ -28,18 +25,19 @@ module.exports = defineConfig({
     ...Object.keys(pkg.peerDependencies),
   ].map((packageName) => new RegExp(`^${packageName}($|/)`, "u")),
   plugins: [
-    nodeResolve({
-      extensions,
-    }),
-    babel({
-      extensions,
-      babelHelpers: "runtime",
-    }),
     typescript({
       exclude: ["**/*.stories.*"],
       filterRoot: "src",
-      outputToFilesystem: true, // Let `tsconfig.tsbuildinfo` be in the root
-      noForceEmit: true,
+    }),
+    getBabelOutputPlugin({
+      presets: [["@babel/preset-env", { bugfixes: true }]],
+      plugins: [
+        [
+          "@babel/plugin-transform-runtime",
+          { version: pkg.dependencies["@babel/runtime"] },
+        ],
+        ["babel-plugin-optimize-clsx", { functionNames: ["clsx"] }],
+      ],
     }),
   ],
 });
