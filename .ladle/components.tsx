b3a4399382d30fad86c5afd147e74e6f95919d96
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:27:45 +0100

    test(ladle): dark mode support

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,1 +1,23 @@
 import "../src/styles.css";
+
+import { GlobalProvider, ThemeState } from "@ladle/react";
+import * as React from "react";
+
+export const Provider: GlobalProvider = function ({ globalState, children }) {
+  const [theme, setTheme] = React.useState<"light" | "dark">();
+  React.useEffect(() => {
+    if (globalState.theme !== ThemeState.Auto) {
+      setTheme(globalState.theme);
+    } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+      setTheme("light");
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme(undefined);
+    }
+  }, [globalState.theme]);
+
+  return (
+    <div className={theme === "dark" ? "dark" : undefined}>{children}</div>
+  );
+};
