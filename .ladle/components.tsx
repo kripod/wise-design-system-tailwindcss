23b014b7cb0f0eddc96046f18dbf7b7146f0f30c
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:31:36 +0100

    test: fix applying theme on popovers

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,16 +1,22 @@
 import "./styles.css";
 
-import { GlobalProvider, ThemeState } from "@ladle/react";
+import { type GlobalProvider, ThemeState } from "@ladle/react";
 import { clsx } from "clsx";
 import * as React from "react";
 
 export const Provider: GlobalProvider = function ({ globalState, children }) {
-  const theme: "light" | "dark" =
-    globalState.theme !== ThemeState.Auto ? globalState.theme : "light";
-
-  return (
-    <React.StrictMode>
-      <div className={clsx(theme === "dark" && "theme-dark")}>{children}</div>
-    </React.StrictMode>
+  const themeClassName = clsx(
+    globalState.theme === ThemeState.Dark && "theme-dark",
   );
+  React.useEffect(() => {
+    if (themeClassName) {
+      document.documentElement.classList.add(themeClassName);
+      return () => {
+        document.documentElement.classList.remove(themeClassName);
+      };
+    }
+    return () => {};
+  }, [themeClassName]);
+
+  return <React.StrictMode>{children}</React.StrictMode>;
 };
