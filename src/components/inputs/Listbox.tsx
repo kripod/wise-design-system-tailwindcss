058c5b330e738c0f26252440dd3867a689f54900
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:29:49 +0100

    feat(Listbox): chevron icon next to selected item

--- src/components/inputs/Listbox.tsx
+++ src/components/inputs/Listbox.tsx
@@ -6,6 +6,7 @@ import {
   useFloating,
 } from "@floating-ui/react-dom";
 import { Listbox as ListboxBase } from "@headlessui/react";
+import { ChevronDown } from "@transferwise/icons";
 import { clsx } from "clsx";
 import { useState } from "react";
 
@@ -53,9 +54,11 @@ export function Listbox() {
           "px-4 text-content-primary ring-1 ring-inset ring-interactive-secondary transition-shadow invalid:ring invalid:!ring-sentiment-negative enabled:hover:ring enabled:hover:ring-interactive-secondary-hover enabled:focus:outline-none enabled:focus:ring-3 enabled:focus:ring-interactive-primary disabled:opacity-45 disabled:mix-blend-luminosity",
           "py-3 text-base",
           "rounded",
+          "inline-flex items-center gap-x-2",
         )}
       >
         {selectedPerson.name}
+        <ChevronDown />
       </ListboxBase.Button>
 
       <ListboxBase.Options
