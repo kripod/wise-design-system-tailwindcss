6104f362a4784fa76edb57c10db6ff17613f3fd3
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:29:20 +0100

    feat(useConstant): allow lazy initialization

--- src/hooks/useConstant.ts
+++ src/hooks/useConstant.ts
@@ -2,12 +2,8 @@ import * as React from "react";
 
 export function useConstant<T>(factory: () => T): T {
   const ref = React.useRef<T>();
-
-  const initialized = React.useRef(false);
-  if (!initialized.current) {
+  if (ref.current === undefined) {
     ref.current = factory();
-    initialized.current = true;
   }
-
   return ref.current as T;
 }
