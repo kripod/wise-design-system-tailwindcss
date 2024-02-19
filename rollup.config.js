285ddfeea66b0cdfcf0bd607f1b15f8f65cc4223
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:32:01 +0100

    feat: emit declaration maps

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,8 @@
 const { babel } = require("@rollup/plugin-babel");
-const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const typescript = require("@rollup/plugin-typescript");
 const { defineConfig } = require("rollup");
 
 const pkg = require("./package.json");
-
-const extensions = [".js", ".jsx", ".mjs", ".ts", ".tsx"];
 
 module.exports = defineConfig({
   input: {
@@ -28,18 +25,14 @@ module.exports = defineConfig({
     ...Object.keys(pkg.peerDependencies),
   ].map((packageName) => new RegExp(`^${packageName}($|/)`, "u")),
   plugins: [
-    nodeResolve({
-      extensions,
-    }),
-    babel({
-      extensions,
-      babelHelpers: "runtime",
-    }),
     typescript({
       exclude: ["**/*.stories.*"],
       filterRoot: "src",
       outputToFilesystem: true, // Let `tsconfig.tsbuildinfo` be in the root
-      noForceEmit: true,
+    }),
+    babel({
+      extensions: [".js", ".jsx", ".mjs", ".ts", ".tsx"],
+      babelHelpers: "runtime",
     }),
   ],
 });
