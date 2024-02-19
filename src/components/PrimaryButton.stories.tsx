7d1301a6efd2f19a5514e49e2f0df3b309e3f7d4
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:27:41 +0100

    test(PrimaryButton): children control for story

--- src/components/PrimaryButton.stories.tsx
+++ src/components/PrimaryButton.stories.tsx
@@ -3,20 +3,24 @@ import type { Story } from "@ladle/react";
 import { PrimaryButton, PrimaryButtonProps } from "./PrimaryButton";
 
 export const Basic: Story<
-  Pick<PrimaryButtonProps, "size" | "sentiment" | "disabled" | "onClick">
-> = ({ size, sentiment, disabled, onClick }) => (
+  Pick<
+    PrimaryButtonProps,
+    "size" | "sentiment" | "disabled" | "children" | "onClick"
+  >
+> = ({ size, sentiment, disabled, children, onClick }) => (
   <PrimaryButton
     size={size}
     sentiment={sentiment}
     disabled={disabled}
     onClick={onClick}
   >
-    Button
+    {children}
   </PrimaryButton>
 );
 
 Basic.args = {
   disabled: false,
+  children: "Click me",
 };
 
 Basic.argTypes = {
