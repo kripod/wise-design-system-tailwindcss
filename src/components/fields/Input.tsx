4e6ff82a2fe3a3d9aa3f763e31151570c2bfcfc8
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:30:01 +0100

    refactor(Input): reorder

--- src/components/fields/Input.tsx
+++ src/components/fields/Input.tsx
@@ -13,65 +13,6 @@ import {
 export const inputPaddingInlineStartAtom = atom<number | undefined>(undefined);
 export const inputPaddingInlineEndAtom = atom<number | undefined>(undefined);
 
-export type InputGroupProps = {
-  children?: React.ReactNode;
-};
-
-export function InputGroup({ children }: InputGroupProps) {
-  return (
-    <Provider>
-      <span className="group/input inline-grid [&>*]:col-start-1 [&>*]:row-start-1">
-        {children}
-      </span>
-    </Provider>
-  );
-}
-
-export type InputAddonProps = {
-  interactive?: boolean;
-  margin?: "none" | "sm" | "md";
-  children?: React.ReactNode;
-};
-
-export function InputAddon({
-  interactive = false,
-  margin = "md",
-  children,
-}: InputAddonProps) {
-  const setInputPaddingInlineStart = useSetAtom(inputPaddingInlineStartAtom);
-  const setInputPaddingInlineEnd = useSetAtom(inputPaddingInlineEndAtom);
-  const setInputPadding = !interactive
-    ? setInputPaddingInlineStart
-    : setInputPaddingInlineEnd;
-
-  const ref = React.useRef<HTMLSpanElement>(null);
-  useResizeObserver(ref, (entry) => {
-    // TODO: Remove condition once most browsers support `borderBoxSize`
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (entry.borderBoxSize != null) {
-      setInputPadding(entry.borderBoxSize[0].inlineSize);
-    }
-  });
-
-  return (
-    <span
-      ref={ref}
-      className={clsx(
-        "pointer-events-none z-10 self-center text-interactive-secondary transition group-[:has(>input:focus:enabled:enabled)]/input:text-interactive-primary group-[:has(>input:hover:enabled)]/input:text-interactive-secondary-hover group-[:has(>input:disabled)]/input:opacity-45 group-[:has(>input:disabled)]/input:mix-blend-luminosity",
-        !interactive
-          ? "justify-self-start"
-          : "justify-self-end [&>*]:pointer-events-auto",
-        {
-          "px-2": margin === "sm",
-          [clsx("px-4", !interactive ? "pe-2" : "ps-2")]: margin === "md",
-        },
-      )}
-    >
-      {children}
-    </span>
-  );
-}
-
 export type InputProps = Merge<
   Pick<
     React.ComponentPropsWithRef<"input">,
@@ -130,3 +71,62 @@ export const Input = React.forwardRef(function Input(
     />
   );
 });
+
+export type InputGroupProps = {
+  children?: React.ReactNode;
+};
+
+export function InputGroup({ children }: InputGroupProps) {
+  return (
+    <Provider>
+      <span className="group/input inline-grid [&>*]:col-start-1 [&>*]:row-start-1">
+        {children}
+      </span>
+    </Provider>
+  );
+}
+
+export type InputAddonProps = {
+  interactive?: boolean;
+  margin?: "none" | "sm" | "md";
+  children?: React.ReactNode;
+};
+
+export function InputAddon({
+  interactive = false,
+  margin = "md",
+  children,
+}: InputAddonProps) {
+  const setInputPaddingInlineStart = useSetAtom(inputPaddingInlineStartAtom);
+  const setInputPaddingInlineEnd = useSetAtom(inputPaddingInlineEndAtom);
+  const setInputPadding = !interactive
+    ? setInputPaddingInlineStart
+    : setInputPaddingInlineEnd;
+
+  const ref = React.useRef<HTMLSpanElement>(null);
+  useResizeObserver(ref, (entry) => {
+    // TODO: Remove condition once most browsers support `borderBoxSize`
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (entry.borderBoxSize != null) {
+      setInputPadding(entry.borderBoxSize[0].inlineSize);
+    }
+  });
+
+  return (
+    <span
+      ref={ref}
+      className={clsx(
+        "pointer-events-none z-10 self-center text-interactive-secondary transition group-[:has(>input:focus:enabled:enabled)]/input:text-interactive-primary group-[:has(>input:hover:enabled)]/input:text-interactive-secondary-hover group-[:has(>input:disabled)]/input:opacity-45 group-[:has(>input:disabled)]/input:mix-blend-luminosity",
+        !interactive
+          ? "justify-self-start"
+          : "justify-self-end [&>*]:pointer-events-auto",
+        {
+          "px-2": margin === "sm",
+          [clsx("px-4", !interactive ? "pe-2" : "ps-2")]: margin === "md",
+        },
+      )}
+    >
+      {children}
+    </span>
+  );
+}
