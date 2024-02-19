c06d28c526f05a8c7662f283443265016140890b
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:31:44 +0100

    refactor(Snackbar): use correct bg color token

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -5,7 +5,7 @@ export type SnackbarProps = {
 
 export function Snackbar({ text, action }: SnackbarProps) {
   return (
-    <div className="theme-invert inline-flex max-w-xl items-center justify-between gap-x-4 rounded bg-contrast-overlay p-4 text-sm text-content-primary shadow">
+    <div className="theme-invert inline-flex max-w-xl items-center justify-between gap-x-4 rounded bg-background-screen p-4 text-sm text-content-primary shadow">
       <div role="status" className="flex-1">
         {text}
       </div>
