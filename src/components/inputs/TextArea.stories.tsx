2b3cb775d80306ed5ec211ac0dbba0eefb26bd9d
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:29:40 +0100

    feat: text area component

---
+++ src/components/inputs/TextArea.stories.tsx
@@ -0,0 +1,39 @@
+import type { Story } from "@ladle/react";
+import * as React from "react";
+
+import { Field, FieldDescription, Label } from "./Field";
+import { TextArea } from "./TextArea";
+
+export const Basic: Story<{
+  required: boolean;
+  readOnly: boolean;
+  disabled: boolean;
+  onClick: () => void;
+}> = function ({ required, readOnly, disabled }) {
+  const [value, setValue] = React.useState("Text value");
+  return (
+    <Field>
+      <Label>
+        Label
+        <TextArea
+          value={value}
+          required={required}
+          readOnly={readOnly}
+          disabled={disabled}
+          onChange={(event) => setValue(event.currentTarget.value)}
+        />
+      </Label>
+      {value.length > 0 ? (
+        <FieldDescription>Information message.</FieldDescription>
+      ) : (
+        <FieldDescription sentiment="negative">Error message.</FieldDescription>
+      )}
+    </Field>
+  );
+};
+
+Basic.args = {
+  required: true,
+  readOnly: false,
+  disabled: false,
+};
---
+++ src/components/inputs/TextArea.tsx
@@ -0,0 +1,28 @@
+import { clsx } from "clsx";
+import { useAtomValue } from "jotai";
+import * as React from "react";
+
+import { fieldDescribedByAtom, fieldInvalidAtom } from "./Field";
+
+export type TextAreaProps = React.ComponentPropsWithRef<"textarea">;
+
+export const TextArea = React.forwardRef(function TextArea(
+  { className, ...restProps }: TextAreaProps,
+  ref: React.ForwardedRef<HTMLTextAreaElement>,
+) {
+  const fieldDescribedBy = useAtomValue(fieldDescribedByAtom);
+  const fieldInvalid = useAtomValue(fieldInvalidAtom);
+
+  return (
+    <textarea
+      ref={ref}
+      aria-describedby={fieldDescribedBy}
+      aria-invalid={fieldInvalid}
+      className={clsx(
+        "rounded px-4 py-3 text-base text-content-primary ring-1 ring-inset ring-interactive-secondary transition-shadow invalid:ring invalid:!ring-sentiment-negative enabled:hover:ring enabled:hover:ring-interactive-secondary-hover enabled:focus:outline-none enabled:focus:ring-3 enabled:focus:ring-interactive-primary disabled:opacity-45 disabled:mix-blend-luminosity",
+        className,
+      )}
+      {...restProps}
+    />
+  );
+});
