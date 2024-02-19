228d5acc7b95960432c2a4ed2b7b77b84781be18
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:28:03 +0100

    perf(Disclosure): descend render prop

--- src/components/Disclosure.tsx
+++ src/components/Disclosure.tsx
@@ -21,14 +21,14 @@ export function Disclosure({
       defaultOpen={initialOpen}
       className="relative flex flex-col gap-y-4 p-4 text-content-primary"
     >
-      {({ open }) => (
-        <>
-          <DisclosureBase.Button
-            className="flex gap-x-4 text-lg font-semibold after:absolute after:inset-0 focus:outline-none focus-visible:ring"
-            style={{
-              textAlign: "start", // TODO: Replace with `text-start`
-            }}
-          >
+      <DisclosureBase.Button
+        className="flex gap-x-4 text-lg font-semibold after:absolute after:inset-0 focus:outline-none focus-visible:ring"
+        style={{
+          textAlign: "start", // TODO: Replace with `text-start`
+        }}
+      >
+        {({ open }) => (
+          <>
             {icon}
             <span className="flex-1">{summary}</span>
             <ChevronDown
@@ -38,13 +38,13 @@ export function Disclosure({
                 open && "-rotate-180",
               )}
             />
-          </DisclosureBase.Button>
-          <DisclosureBase.Panel className="relative -m-4 mt-0 flex gap-x-4 p-4 pt-0 text-base text-content-secondary">
-            {icon != null ? <span className="invisible">{icon}</span> : null}
-            <div className="flex-1">{children}</div>
-          </DisclosureBase.Panel>
-        </>
-      )}
+          </>
+        )}
+      </DisclosureBase.Button>
+      <DisclosureBase.Panel className="relative -m-4 mt-0 flex gap-x-4 p-4 pt-0 text-base text-content-secondary">
+        {icon != null ? <span className="invisible">{icon}</span> : null}
+        <div className="flex-1">{children}</div>
+      </DisclosureBase.Panel>
     </DisclosureBase>
   );
 }
