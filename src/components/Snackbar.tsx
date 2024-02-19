96f8237eb2d0fc7270f3a36a628a4d7403275260
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:31:33 +0100

    refactor(Snackbar): simplify

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -5,7 +5,7 @@ export type SnackbarProps = {
 
 export function Snackbar({ action, children }: SnackbarProps) {
   return (
-    <div className="inline-flex min-w-full items-center justify-between gap-x-4 rounded bg-contrast-theme p-4 text-contrast-overlay shadow desktop:min-w-0">
+    <div className="inline-flex items-center justify-between gap-x-4 rounded bg-contrast-theme p-4 text-contrast-overlay shadow">
       <div className="max-w-md flex-1 text-sm">{children}</div>
       {action != null ? <div className="-my-2">{action}</div> : null}
     </div>
