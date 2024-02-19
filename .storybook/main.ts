c5bd4ce6414dd763feb746fb7a40961aa8b2c03b
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:34:52 +0100

    chore(storybook): disable telemetry

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,13 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 export default {
+  stories: ["../src/"],
   framework: {
     name: "@storybook/react-vite",
     options: {
       strictMode: true,
     },
   },
-  stories: ["../src/"],
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-essentials",
@@ -15,4 +15,7 @@ export default {
     "@storybook/addon-links",
     "@storybook/addon-themes",
   ],
+  core: {
+    disableTelemetry: true,
+  },
 } satisfies StorybookConfig;
