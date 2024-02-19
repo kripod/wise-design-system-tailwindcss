783c678157ddde1e2213521629b8ad6de7ada734
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:33:10 +0100

    test(Flag): add story

---
+++ src/components/Flag.stories.tsx
@@ -0,0 +1,15 @@
+import type { Story } from "@ladle/react";
+
+import { Flag } from "./Flag";
+
+export const Basic: Story<{
+  code: string;
+  size: number;
+}> = function ({ code, size }) {
+  return <Flag code={code} size={size} />;
+};
+
+Basic.args = {
+  code: "usd",
+  size: 64,
+};
