bd4cf78024f8a80cca3c3d5fd5d4135ab6f3c902
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:33:23 +0100

    fix(forwardRefWithGenerics): interop with React 18

--- src/forwardRefWithGenerics.ts
+++ src/forwardRefWithGenerics.ts
@@ -1,10 +1,11 @@
 import * as React from "react";
 
-/* TODO: Replace `React.ReactElement | null` with `React.ReactNode` when adopting React 18 */
-
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const forwardRefWithGenerics = React.forwardRef as <T, P = {}>(
-  render: (props: P, ref: React.ForwardedRef<T>) => React.ReactElement | null,
+  render: (
+    props: P,
+    ref: React.ForwardedRef<T>,
+  ) => ReturnType<React.FunctionComponent>,
 ) => (
   props: React.PropsWithoutRef<P> & React.RefAttributes<T>,
-) => React.ReactElement | null;
+) => ReturnType<React.FunctionComponent>;
