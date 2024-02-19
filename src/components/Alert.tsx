4960dc7fc9dcc3d5b3cc9a99cba94eda77ac262d
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-02-19 17:28:17 +0100

    fix(Alert): element with role

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -37,12 +37,12 @@ export function Alert({
 }: AlertProps) {
   const Icon = iconBySentiment[sentiment];
   return (
-    <div
-      role="alert"
-      className="grid grid-cols-[auto_1fr_auto] items-center gap-4 rounded-lg bg-background-neutral p-6 text-content-secondary"
-    >
+    <div className="grid grid-cols-[auto_1fr_auto] items-center gap-4 rounded-lg bg-background-neutral p-6 text-content-secondary">
       <Icon />
-      <div className="col-span-full row-start-2 space-y-2 text-sm desktop:col-span-1 desktop:row-start-auto">
+      <div
+        role="alert"
+        className="col-span-full row-start-2 space-y-2 text-sm desktop:col-span-1 desktop:row-start-auto"
+      >
         {children}
       </div>
       {onClose != null ? (
