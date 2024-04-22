2e0593fad82aaceee4b89adfea6e2225a271027a
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-04-22 06:34:35 +0200

    refactor(number): remove unused 'safeParseFloat'

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -7,13 +7,3 @@ const MAX_SAFE_PRECISION = Math.trunc(
 export function closestSafeFloat(value: number) {
   return Number(value.toPrecision(MAX_SAFE_PRECISION));
 }
-
-export function safeParseFloat(value: string) {
-  if (value.trim()) {
-    const parsed = Number(value);
-    if (Number.isFinite(parsed)) {
-      return parsed || 0; // Treat `-0` as `0`
-    }
-  }
-  return null;
-}
