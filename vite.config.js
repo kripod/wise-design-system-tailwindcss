f7325d24609bb6d0b41edb818276f5d328c6dd04
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-05-25 22:23:55 +0200

    build: migrate to Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,50 @@
 import react from "@vitejs/plugin-react-swc";
+import browserslist from "browserslist";
+import swc from "unplugin-swc";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: "css-homogenizer/reset-scoped/react",
-    }),
-  ],
-});
+import pkg from "./package.json" with { type: "json" };
+
+const jsxImportSource = "css-homogenizer/reset-scoped/react";
+
+export default defineConfig((env) => ({
+  plugins:
+    env.mode !== "production"
+      ? [react({ jsxImportSource })]
+      : [
+          swc.vite({
+            tsconfigFile: false,
+            env: {
+              targets: browserslist(),
+              bugfixes: true,
+            },
+            jsc: {
+              externalHelpers: true,
+              transform: {
+                react: {
+                  runtime: "automatic",
+                  importSource: jsxImportSource,
+                },
+              },
+            },
+          }),
+          dts(),
+        ],
+  build: {
+    lib: {
+      entry: ["./src/index.ts", "./src/tailwind-base.ts"],
+    },
+    sourcemap: true,
+    minify: false,
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies ?? {}),
+        ...Object.keys(pkg.peerDependencies ?? {}),
+      ].map((packageName) => new RegExp(`^${packageName}($|/)`, "u")),
+      output: {
+        preserveModules: true,
+      },
+    },
+  },
+}));
